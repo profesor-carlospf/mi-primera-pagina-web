1f72f6d1ac73dca5bf07692b19b17dc998cc2353
Carlos Pareja <profesor.carlospf@ies-azarquiel.es>
2022-12-20 10:03:55 +0100

    ejercicios de swicth

---
+++ site/js/suma-y-resta.js
@@ -0,0 +1,10 @@
+var primerNumero = parseInt(prompt("Introduzca el primer número: "),10);
+var segundoNumero = parseInt(prompt("Introduzca el segundo número: "),10);
+
+var suma = primerNumero + segundoNumero;
+var resta = primerNumero - segundoNumero;
+
+alert("La suma de los dos números es: " + suma);
+document.write("La suma de los dos números es: " + suma);
+alert("La resta de los dos números es: " + resta);
+document.write("<br>La resta de los dos números es: " + resta);
